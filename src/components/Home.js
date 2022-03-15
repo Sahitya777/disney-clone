eb7bdf248ef6784726f89fc3227c309697884b58
Sahitya77 <sahityanijhawan@gmail.com>
2022-03-15 15:56:48 +0530

    FireBase inited!!

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import styled from 'styled-components'
 import ImgSlider from './ImgSlider'
 import Viewers from './Viewers'
 import Movies from './Movies'
+
 function Home() {
+
+  useEffect(()=>{
+    console.log("hello");
+  },[])
+
   return (
     <Container>
         <ImgSlider/> 
